fab8a1f831f60d7ea161e8883078025238286a77
sarthakrana21 <sarthak.rana0321@gmail.com>
2025-01-22 13:12:27 +0530

    added tailwind css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,17 @@
-import { useRef } from "react"
-
+import { useRef, useState } from "react"
+import Loader from "./Loader";
 
 const Home = () => {
+  const [loading, setLoading] =useState<boolean>(true)
   let context: string | null;
   const textAreaRef = useRef<HTMLDivElement | null>(null)
   const resultAreaRef = useRef<HTMLPreElement | null>(null)
 
   const onSubmit = async () => {
-  
-
+    setLoading(true)
     if(textAreaRef.current) {
       context = textAreaRef.current.innerHTML
+      textAreaRef.current.innerHTML = '';
     }
 
     if(chrome && chrome.runtime){
@@ -33,10 +34,9 @@ const Home = () => {
     }
     
   } 
+
   const setResponse = (response: string) => {
-    if(textAreaRef.current  ) {
-      textAreaRef.current.innerHTML = '';
-    }
+    setLoading(false)
     if(resultAreaRef.current) {
       console.log(response)
       const formatedText = response.replace(/\\n/g, '\n');
@@ -45,15 +45,21 @@ const Home = () => {
   }
 
   return (
-    <div className='container'>
-        <h2>Update Master AI</h2>
-        <div className="main-area">
+    // Root Container
+    <div className='w-full h-full flex flex-col items-center justify-between'>
+        <h1 className="text-2xl font-semibold mt-4">Update Master ai</h1>
+        {/* Main container */}
+        <div className="flex flex-col h-full w-full items-center justify-between">
+          <div className="absolute z-20 top-40">
+            {loading && <Loader />}
+          </div>
+          {/* result Area */}
+          <pre className="block max-w-[320px] min-w-[320px] h-[300px] overflow-auto whitespace-pre-wrap cursor-text break-words my-5 z-9 bg-[#333333] p-[7px] px-2.5 rounded-lg" ref={resultAreaRef} aria-placeholder="Welcome to Update MAster AI.." />
 
-          <pre className="result-area" ref={resultAreaRef} aria-placeholder="Welcome to Update MAster AI.." />
-    
-          <div className="input-container">
+          {/* User Input Area */}
+          <div className="flex flex-col-reverse w-full items-center mb-5">
             <div 
-            className="textarea" 
+            className="absolute block w-80 min-h-[20px] max-h-[150px] border border-whitesmoke overflow-auto outline-none rounded-lg p-[7px] px-2.5 cursor-text break-words z-10 bg-[#2b2b2b]" 
             contentEditable="plaintext-only"
             ref={textAreaRef}
             aria-placeholder="Enter your message here!"
@@ -61,8 +67,9 @@ const Home = () => {
           </div>
 
         </div>
-        <div className="button-area">
-          <button className="send-btn" onClick={onSubmit}>Send</button>
+        {/* Buttons section */}
+        <div className="">
+          <button className="rounded-lg border border-transparent px-5 py-2 text-base font-medium bg-black text-sm text-white cursor-pointer transition-colors duration-200 mb-5 hover:border-indigo-500" onClick={onSubmit}>Send</button>
         </div>
     </div>
   )
